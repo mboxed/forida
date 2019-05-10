f44dfa909782c64ddeea8ffee4d21ae3c9388b78
mboxed <mboxxed@gmail.com>
2019-05-09 20:02:48 -0400

    webring icon

--- scripts/graph.js
+++ scripts/graph.js
@@ -57,6 +57,7 @@ RIVEN.create = (append = true) => {
       <a target='_blank' href="https://twitter.com/mboxxed" class="icon twitter external"></a>
       <a target='_blank' href="https://github.com/mboxed" class="icon github external"></a>      
       <a target='_blank' href="https://merveilles.town/@m" class="icon merveilles external"></a>
+      <a target='_blank' href="http://webring.xxiivv.com/#random" class="icon rotonde"></a>
       <a target='_blank' href="https://creativecommons.org/licenses/by-nc-sa/4.0/" class="icon cc"></a>
       <a data-goto='m' href='#m'>M</a> © ${new Arvelie('19I14').toString(true)}—${arvelie()}
       <center>${'About'.toLink('BY-NC-SA 4.0')} <span style="color:#ccc"'>${neralie()}</span></center>
